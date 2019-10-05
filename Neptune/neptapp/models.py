97c10224b5a22feee6783a17819139fdadac7040
Curiouspaul1 <170408025@live.unilag.edu.ng>
2019-10-05 02:58:29 +0100

    Added database models

--- Neptune/neptapp/models.py
+++ Neptune/neptapp/models.py
@@ -1,1 +1,26 @@
+from neptapp import db,ma
+
+class Tutors(db.Model):
+    id = db.Column(db.Integer)
+    name = db.String(200,nullable=False)
+    email = db.String(100)
+    mskill = db.String(200)
+    oskill = db.String(200)
+    description = db.Text()
+    uid = db.Column(db.String(200))
+
+class Students(db.Model):
+    id = db.Column(db.Integer)
+    uid = db.Column(db.String(200))
+    name = db.String(200)
+    email = db.String(200)
+
+# Schema
+class TutorSchema(ma.Schema):
+    class Meta:
+        fields = ('id','name','email','mskill','oksill','description','uid')
+
+class StudentSchema(ma.Schema):
+    class Meta:
+        fields = ('id','uid','name','email')
 
--- Neptune/neptapp/views.py
+++ Neptune/neptapp/views.py
@@ -1,1 +1,24 @@
+from neptapp import app,db
+from flask import json,jsonify
 
+@app.route('/tsignup',methods=['GET','POST'])
+def tsignup():
+    if request.method == 'POST':
+        name = request.json['name']
+        email = request.json['email']
+        mskill = request.json['mskill']
+        oskill = request.json['oskill']
+        desc = request.json['desc']
+        uid = request.json['uid']
+
+        tutor = Tutors(name=name,emil=email,mskill=mskill,oskill=oskill,description=desc,uid=uid)
+        db.session.add(tutor)
+        db.session.commit()
+
+
+@app.route('/lsinup',methods=['GET,'POST])
+def lsignup():
+    if request.method=='POST':
+        name = request.json['name']
+        email = request.json['email']
+        uid = request.json['uid']
