c82af567a882f3d57157f8913517817f0d38f084
Curiouspaul1 <170408025@live.unilag.edu.ng>
2019-10-05 09:50:15 +0100

    Added Piplock file

--- Neptune/neptapp/models.py
+++ Neptune/neptapp/models.py
@@ -2,30 +2,32 @@ from neptapp import db,ma
 
 class Tutors(db.Model):
     id = db.Column(db.Integer)
-    name = db.String(200,nullable=False)
+   # name = db.String(200,nullable=False)
     email = db.String(100)
     mskill = db.String(200)
     oskill = db.String(200)
     description = db.Text()
-    uid = db.Column(db.String(200))
+    uid = db.Column(db.String(200),primary_key=True)
+    role = db.Column(db.String(100))
 
 class Students(db.Model):
     id = db.Column(db.Integer)
-    uid = db.Column(db.String(200))
+    uid = db.Column(db.String(200),primary_key=True)
     name = db.String(200)
     email = db.String(200)
+    role = db.Column(db.String(100))
 
 # Schema
-class TutorSchema(ma.Schema):
+class TutorSchema(ma.ModelSchema):
     class Meta:
-        fields = ('id','name','email','mskill','oksill','description','uid')
+        model = Tutors
 
-class StudentSchema(ma.Schema):
+class StudentSchema(ma.ModelSchema):
     class Meta:
-        fields = ('id','uid','name','email')
+        model = Students
 
 
 # Init Schema
-Tutor_schema = TutorSchema(strict=True)
-Tutors_schema = TutorSchema(many=True,strict=True)
+Tutor_schema = TutorSchema()
+Tutors_schema = TutorSchema(many=True)
 
--- Neptune/neptapp/views.py
+++ Neptune/neptapp/views.py
@@ -11,8 +11,9 @@ def tsignup():
         oskill = request.json['oskill']
         desc = request.json['desc']
         uid = request.json['uid']
+        role = request.json['role']
 
-        tutor = Tutors(name=name,emil=email,mskill=mskill,oskill=oskill,description=desc,uid=uid)
+        tutor = Tutors(name=name,emil=email,mskill=mskill,oskill=oskill,description=desc,uid=uid,role=role)
         db.session.add(tutor)
         db.session.commit()
 
@@ -22,15 +23,24 @@ def tsignup():
 def index():
     return "<h1>Hello World</h1>"
 
-@app.route('/lsignup',methods=['GET,'POST])
+@app.route('/lsignup',methods=['GET','POST'])
 def lsignup():
     if request.method=='POST':
         name = request.json['name']
         email = request.json['email']
         uid = request.json['uid']
+        role = request.role['uid']
 
-        student = Students(name=name,email=email,uid=uid)
+        student = Students(name=name,email=email,uid=uid,role=role)
         db.session.add(student)
         db.session.commit()
 
     return Tutor_schema.jsonify(student)
+
+# get single user
+
+@app.route('/get-user',methods=['GET','POST'])
+def getUser():
+    uid = request.json['uid']
+    userx = Tutors.query.filter_by(uid=uid).first()
+    return Tutor_schema.jsonify(userx)
